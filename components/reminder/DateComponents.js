57d97362a361d68189bf6388e0615d473d4995ab
Damian Semler <damiansemler@Damians-MacBook-Pro.local>
2018-11-18 22:10:07 +0000

    refactor

--- components/reminder/DateComponents.js
+++ components/reminder/DateComponents.js
@@ -1,15 +1,11 @@
 import { View, Text } from 'react-native';
 import styled from 'styled-components';
 
-
-export const DateContainer = styled(View)`
-  padding-top: 23px;
-`;
-
 export const DayOfTheWeekText = styled(Text)`
   font-family: 'SF-Pro-Display-Bold';
   font-size: 30em;
   padding-left: 22px;
+  margin-top: 23px;
   padding-top: 13px;
   padding-bottom: 13px;
 `;
