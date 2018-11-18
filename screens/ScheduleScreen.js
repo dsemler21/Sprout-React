98e27936a0bd8ec62b8c0618381928f94deaa331
Damian Semler <damiansemler@Damians-MacBook-Pro.local>
2018-11-18 23:37:07 +0000

    Grouped reminders by Day of the week

--- screens/ScheduleScreen.js
+++ screens/ScheduleScreen.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { ScrollView } from 'react-native';
 import styled from 'styled-components';
-import { DateContainer, DayOfTheWeekText } from '../components/reminder/DateComponents';
+import { DayOfTheWeekText } from '../components/reminder/DateComponents';
 import Reminder from '../components/reminder/Reminder';
+import { resetTime, getDayStringFromDayIndex, dateEquals } from '../utils/dateUtils';
+
+// TODO : should be retrieve from a server by a Service file or something
+const testData = require('../example-data/reminders.json');
 
 const ScheduleView = styled(ScrollView)`
     flex: 1;
@@ -25,10 +29,51 @@ export default class ScheduleScreen extends React.Component {
   render() {
     return (
       <ScheduleView>
-        <DayOfTheWeekText>Today</DayOfTheWeekText>
-        <Reminder name="Juliana" />
-        <Reminder name="Beatrix" />
+        {this.renderRemindersByDay()}
       </ScheduleView>
     );
   }
+
+  renderRemindersByDay() {
+    const remindersGroupedByDay = ScheduleScreen.groupRemindersByDate(testData);
+    return remindersGroupedByDay.map(
+      (day) => (
+        <>
+          <DayOfTheWeekText>{day.dayString}</DayOfTheWeekText>
+          {this.renderRemindersInDay(day)}
+        </>
+      )
+    );
+  }
+
+  renderRemindersInDay(day) {
+    return day.reminders.map((reminder) => (<Reminder name={reminder.name} />));
+  }
+
+  /**
+   * @param data [{date: DateString, name: string}] **IMPORTANT** it assumes that data is ordered by date, ascending
+   * // TODO: order it by ourselves {DO NOT TRUST ANY ONE}
+   * @return array of reminders grouped by dates according to [{ dayString: string, reminders: [Reminder] }]
+   */
+  static groupRemindersByDate(data) {
+    if (data.length === 0)
+      // TODO: return a real designed view
+      return (
+        <Text>It seems you haven’t added any plants yet tap the “add” button above to start</Text>
+      );
+    const days = [];
+    for (let i = 0; i < data.length;) {
+      let actualDay = resetTime(new Date(data[i].date));
+      let day = {
+        dayString: getDayStringFromDayIndex(actualDay),
+        reminders: []
+      };
+      for (; i < data.length && dateEquals(resetTime(new Date(data[i].date)), actualDay); ++i) {
+        day.reminders.push(data[i]);
+      }
+      days.push(day);
+    }
+    return days;
+  }
+
 }
