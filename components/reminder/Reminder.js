62cb10e770fa83517e157b998b1a87956bed52eb
Damian Semler <damiansemler@Damians-MacBook-Pro.local>
2018-11-19 12:47:53 +0000

    Added styling to reminder container & text

--- components/reminder/Reminder.js
+++ components/reminder/Reminder.js
@@ -3,11 +3,16 @@ import { View, Text } from 'react-native';
 import styled from 'styled-components';
 
 const ReminderContainer = styled(View)`
-    
+    height: 64px;
+    border: 1px solid #E6E6E6;
 `;
 
 const ReminderText = styled(Text)`
-
+    font-family: 'SF-Pro-Display-Regular';
+    font-size: 18px;
+    padding-left: 15px;
+    padding-top: 22px;
+    padding-bottom: 22px;
 `;
 
 export default class Reminder extends React.Component {
